4934705ab7d6361a933fed4bdf8e9eb994cc0d95
yuvrajyk <kagilkaryuvi@gmail.com>
2020-02-08 22:05:08 +0530

    first commit

---
+++ practice.php
@@ -0,0 +1,9 @@
+<?php
+
+$num=1;
+while($num < 10){
+echo $num; echo "</br>";
+$num++;
+}
+
+?>
---
+++ practice1.php
@@ -0,0 +1,35 @@
+<?php
+
+$a=10;
+$b=20;
+
+echo $a."<br>";
+echo $b."<br>";
+$a++;
+$b--;
+
+echo $a."<br>";
+echo $b."<br>";
+
+echo "************************************************************************";
+
+// basic practice
+$c=($b>$a)?"you are wrong $b":"correct buddy go ahead";
+echo "<br>".$c;
+
+echo "<br>************************************************************************\n\t";
+
+
+$number=2;
+
+if($number % 2==0){
+    $result="EVEN Number Enterd";
+
+}else{
+    $result="ODD Number Enterd";
+}
+
+echo "<br><h4>".$result;echo "</h4>";
+echo "<br>****************************************"
+
+?>
