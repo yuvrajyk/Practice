d9f9a09a73c30e01ca542ede22c271ddb4f24e85
yuvrajyk <kagilkaryuvi@gmail.com>
2020-02-09 20:09:52 +0530

    loop Practice

---
+++ do_while.php
@@ -0,0 +1,11 @@
+<?php
+$i=1;
+$pro=1;
+
+do{
+    $pro=$pro*$i;
+    $i++;
+}while($i<=10);
+
+echo "<br>".$pro;
+?>
--- while.php
+++ while.php
@@ -3,10 +3,40 @@
 $i=1;
 $sum=0;
 
-while($i<=10){
+while($i<=5){
     $sum=$sum+$i;
     $i=$i+1;
+    // echo $sum;die();
 }
 
 echo $sum;
+
+echo "<br>**************************************************************";
+
+//while
+
+$i=10;
+$sum=0;
+while($i>=1){
+    $sum=$sum+$i;
+    $i=$i-1;
+    // echo $i;exit;
+}
+echo "<br>".$sum;
+
+echo "<br>******************* Nested while loop ******************************************";
+
+$i=1;
+$j=1;
+
+while($i<=5){
+    $j=1;
+    while($j<=5){
+        echo "*";
+        $j=$j+1;
+    }
+echo "<br>";
+$i=$i+1;
+}
+
 ?>
